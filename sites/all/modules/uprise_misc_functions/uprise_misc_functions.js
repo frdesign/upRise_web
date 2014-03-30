f544739c5e234f0b9d34a2b5ef0ffeb1183adeaf
frdesign <frdesign212@gmail.com>
2014-03-30 02:54:01 -0400

    Content types set up
    Web Links, Video & Posts set up. Term of service done. Next step is to
    finish user profile page & design UX for posting content

--- sites/all/modules/uprise_misc_functions/uprise_misc_functions.js
+++ sites/all/modules/uprise_misc_functions/uprise_misc_functions.js
@@ -3,14 +3,26 @@
   // Jquery onload function.
   $(document).ready(function(){
     
-    //Make node links into buttons
-    $(".node-full .node-links a").addClass("typo-btn btn-danger typo-btn-sm btn-sm-danger");
-    
-    //Make comment reply link into button
-    $("li.comment-reply a").addClass("typo-btn btn-danger typo-btn-sm btn-sm-danger");
     
     //Change "Read more" link to Play video for video nodes
-    $('.node-video li.node-readmore a').text('Watch video');
+    $('.node-video li.node-readmore a').text('> Watch video');
+    
+    // Make videos blue in homepage
+    $('.node-teaser.node-video').parent().parent().addClass('tb-wall-black-background') // tb-wall-double-style
+    
+    //Remove colorbox functionality on teasers
+    $('.node-teaser a').removeClass('colorbox-load init-colorbox-load-processed cboxElement');
+    
+    // Hide the first link which is the user provider url to force users to "read more" before leaving site
+    $('.node-web-link.node-teaser div.field-type-text-with-summary a').hide();
+    
+    // Deactivate links in web-link content type when
+    $('.node-web-link.node-teaser div.field-type-text-with-summary div.opengraph-filter a').replaceWith(function() {
+        return this.innerHTML;
+    });
+    
+    // Open Web Links in new window
+    $('.node-web-link div.field-type-text-with-summary a').attr('target','_blank');
     
    });
 })(jQuery); 
