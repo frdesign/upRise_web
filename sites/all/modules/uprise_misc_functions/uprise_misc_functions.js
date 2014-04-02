5cfd060a0f01ce4d3ada6401be61313c7a3d25f0
frdesign <frdesign212@gmail.com>
2014-04-02 00:28:56 -0400

    006 Pretest backup
    Backup before cleaning up menus

--- sites/all/modules/uprise_misc_functions/uprise_misc_functions.js
+++ sites/all/modules/uprise_misc_functions/uprise_misc_functions.js
@@ -7,17 +7,23 @@
     //Change "Read more" link to Play video for video nodes
     $('.node-video li.node-readmore a').text('Watch video');
     
-    // Make videos blue in homepage
-    $('.node-teaser.node-video').parent().parent().addClass('tb-wall-black-background') // tb-wall-double-style
+    //Change "Read more" link to Rate for quotes nodes
+    $('.node-quotes li.node-readmore a').text('Rate');
+    
+    // Make videos black in homepage
+    $('.node-teaser.node-video').parent().parent().addClass('tb-wall-blue-background') // tb-wall-double-style
+    
+    // Make quotes blue in homepage
+    $('.node-teaser.node-quotes').parent().parent().addClass('tb-wall-black-background') // tb-wall-double-style
     
     //Remove colorbox functionality on teasers
     $('.node-teaser a').removeClass('colorbox-load init-colorbox-load-processed cboxElement');
     
     // Hide the first link which is the user provider url to force users to "read more" before leaving site
-    $('.node-web-link.node-teaser div.field-type-text-with-summary a').hide();
+    $('.node-web-link.node-teaser div.field-type-text a').hide();
     
     // Deactivate links in web-link content type when
-    $('.node-web-link.node-teaser div.field-type-text-with-summary div.opengraph-filter a').replaceWith(function() {
+    $('.node-web-link.node-teaser div.field-type-text div.opengraph-filter a').replaceWith(function() {
         return this.innerHTML;
     });
     
@@ -31,10 +37,10 @@
     $('.sf-main-menu .sf-item-3 a').addClass('colorbox-load');
     
     // Change profile "view" tabl to just "profile"
-    $('.page-user ul.tabs li:first a').text('Profile');
+    $('body.logged-in.page-user ul.tabs li:first a').text('Profile');
     
     // Change profile "track" tab to just "Posts"
-    $('.page-user ul.tabs li:nth-child(4) a').text('Posts');
+    $('body.logged-in.page-user ul.tabs li:nth-child(4) a').text('Posts');
     
    });
 })(jQuery); 
